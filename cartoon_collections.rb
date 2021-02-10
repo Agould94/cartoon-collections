5842df70cabadbbe01cae978923426f0b31b71f3
Alexander Gould <alexander.gould94@gmail.com>
2021-02-09 16:45:44 -0800

    commit tree 1269fe68ed0cc335431e9e8afc6ab8d29f1286bf

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(dwarf_names)
+  dwarf_names.each.with_index(1) do |name, index|
+    puts "#{index}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  new_calls = planeteer_calls.collect do |element|
+    "#{element.capitalize}!"
+  end
+    return new_calls
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(assorted_calls)
+  assorted_calls.any? do |word|
+    word.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(string)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.detect do |cheese|
+    string.include?(cheese)
+  end
 end
